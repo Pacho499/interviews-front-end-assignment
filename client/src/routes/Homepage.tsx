217b4fa215556ef5da75317bfcae6ecb022d375d
Lorenzo Palumbo <lorenzopalumbo.dev@gmail.com>
2024-07-15 22:16:06 +0200

    feat: logic context and fetchRecipe

--- client/src/routes/Homepage.tsx
+++ client/src/routes/Homepage.tsx
@@ -1,10 +1,36 @@
-import "../App.css";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import { Recipe } from "../types/apiResponse";
+import { getRecipes } from "../utils/api.ts";
+import RecipeCard from "../components/RecipeCard.tsx";
 
 const Homepage = () => {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const newRecipes = await getRecipes({ page });
+      setRecipes([...recipes, ...newRecipes]);
+    };
+
+    fetchRecipes();
+  }, [page]);
+
+  const RenderRecipeCards = () => {
+    return recipes.map((recipe, id) => {
+      return <RecipeCard key={id} recipe={recipe} />;
+    });
+  };
+
   return (
     <div>
       <Header />
+      <section>
+        <h1>Our recipes</h1>
+        {RenderRecipeCards()}
+      </section>
+      <button onClick={() => setPage(page + 1)}>cerca pagine</button>
     </div>
   );
 };
